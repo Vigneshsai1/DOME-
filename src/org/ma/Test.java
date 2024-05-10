0cb1ab86f8fa643caacca9301740b38701b48afc
vigneshsai1 <vigneshjeyapal104@gmail.com>
2024-05-10 05:27:19 -0700

    thered

--- src/org/ma/Test.java
+++ src/org/ma/Test.java
@@ -1,5 +1,12 @@
 package org.ma;
 
 public class Test {
-
+	public static void main(String[] args) {
+		
+		System.out.println("vicky");
+		System.out.println("name");
+		
+		int s =0;
+		System.out.println(s);
+	}
 }
